a1ec31f2dcf3c03502d352751827503974462520
Mateusz Maćkowski <m4tx@m4tx.pl>
2024-04-17 18:47:03 +0100

    feat: add EasyEffects config

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# thinkpad-p14s-g4-linux
-Various configs for Lenovo ThinkPad P14s G4 running under GNU/Linux distributions
+<p align="center">
+    <img src="images/thonkpad.gif" />
+</p>
+
+# ThinkPad P14s Gen 4 — Linux
+
+Various configs for Lenovo ThinkPad P14s Gen 4 running under GNU/Linux distributions.
+
+## Sound
+
+The default speaker sound is terrible, so the repository includes an [EasyEffects](https://github.com/wwmm/easyeffects) preset to make it better. This includes a mix of a modified LoudnessEqualizer preset from [https://github.com/Digitalone1/EasyEffects-Presets](https://github.com/Digitalone1/EasyEffects-Presets) and a Music/Movies convolver effect config taken from P14s G3 Windows installation. To install the presets, just run the following command:
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/m4tx/thinkpad-p14s-g4-linux/master/install_easyeffects.sh)"
+```
---
+++ install_easyeffects.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Based on install.sh from the EasyEffects-Presets repo by JackHack96, licensed under MIT
+# This script automatically detect the EasyEffects presets directory and installs the presets
+
+GIT_REPOSITORY="https://raw.githubusercontent.com/m4tx/thinkpad-p14s-g4-linux/master"
+
+check_installation() {
+    if command -v flatpak &>/dev/null && flatpak list | grep -q "com.github.wwmm.easyeffects"; then
+        PRESETS_DIRECTORY="$HOME/.var/app/com.github.wwmm.easyeffects/config/easyeffects"
+    elif which easyeffects >/dev/null; then
+        PRESETS_DIRECTORY="$HOME/.config/easyeffects"
+    else
+        echo "Error! Couldn't find EasyEffects presets directory!"
+        exit 1
+    fi
+    mkdir -p "$PRESETS_DIRECTORY"
+}
+
+check_impulse_response_directory() {
+    if [ ! -d "$PRESETS_DIRECTORY/irs" ]; then
+        mkdir "$PRESETS_DIRECTORY/irs"
+    fi
+}
+
+install_presets() {
+    echo "Installing impulse response files..."
+    curl "$GIT_REPOSITORY/easyeffects/T14S_G4_Music_Movies.irs" --output "$PRESETS_DIRECTORY/irs/T14S_G4_Music_Movies.irs" --silent
+    echo "Installing ThinkPad P14s G4 preset..."
+    curl "$GIT_REPOSITORY/easyeffects/ThinkPad%20P14s%20G4.json" --output "$PRESETS_DIRECTORY/output/ThinkPad P14s G4.json" --silent
+    sed -i "s|<PRESETS_DIRECTORY>|$PRESETS_DIRECTORY|g" "$PRESETS_DIRECTORY/output/ThinkPad P14s G4.json"
+}
+
+check_installation
+check_impulse_response_directory
+install_presets
